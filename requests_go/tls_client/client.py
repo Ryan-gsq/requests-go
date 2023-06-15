2a233fbc73efc71c34eacc096a3e6f3daf3f1f15
郭书勤 <my943813636@gmail.com>
2023-06-15 17:20:22 +0800

    update

--- requests_go/tls_client/client.py
+++ requests_go/tls_client/client.py
@@ -5,15 +5,8 @@ from platform import machine
 
 if platform == 'darwin':
     file_ext = '-arm64.dylib' if machine() == "arm64" else '-x86.dylib'
-elif platform in ('win32', 'cygwin'):
-    file_ext = '-win64.dll' if 8 == ctypes.sizeof(ctypes.c_voidp) else '-win32.dll'
 else:
-    if machine() == "aarch64":
-        file_ext = '-arm64.so'
-    elif "x86" in machine():
-        file_ext = '-amd64.so'
-    else:
-        file_ext = '-amd64.so'
+    file_ext = '-arm64.so'
 
 root_dir = os.path.abspath(os.path.dirname(__file__))
 library = ctypes.cdll.LoadLibrary(f'{root_dir}/dependencies/requests-go{file_ext}')
